66e0bc9999ec6512f4153bfa251f60133e38bd02
alex <alexanderlopez@outlook.com>
2018-12-03 14:39:39 -0600

    Commit initial

---
+++ app/src/main/java/com/alex/teknigram/view/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.alex.teknigram.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alex.teknigram.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
